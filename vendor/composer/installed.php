92e7e167ade8a57eeb0e66ba492f9f6d07eb3a43
Spyridon Kavvadas <kavvadasspyridon@gmail.com>
2021-06-05 15:01:08 +0200

    composer.lock added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a710890929855e19cce0767e455cd92edc7c3faf',
+        'name' => 'processwire/processwire',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'processwire/processwire' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a710890929855e19cce0767e455cd92edc7c3faf',
+            'dev_requirement' => false,
+        ),
+    ),
+);
